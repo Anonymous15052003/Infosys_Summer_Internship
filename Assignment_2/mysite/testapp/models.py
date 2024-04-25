c2475a3a74cdb9b5631cad93274b27736b4b3796
Anonymous15052003 <sharonpro24x7@gmail.com>
2024-04-25 19:07:52 +0530

    done with setting up database

--- Assignment_2/mysite/testapp/models.py
+++ Assignment_2/mysite/testapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Employees(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    Dept_name = models.CharField(max_length=20)
+    joining_date = models.DateField()
+    salary = models.IntegerField()
--- Assignment_2/mysite/testapp/views.py
+++ Assignment_2/mysite/testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from testapp.models import Employees
 
 # Create your views here.
+def index(request):
+    emp_list = Employees.objects.all().count()
+    return HttpResponse(emp_list)
