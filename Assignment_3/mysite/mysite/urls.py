41de3ea06e81d3c7b4a2b5d015274b3ea120cd73
Anonymous15052003 <sharonpro24x7@gmail.com>
2024-04-25 19:31:54 +0530

    included all the required code

--- Assignment_3/mysite/mysite/urls.py
+++ Assignment_3/mysite/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp import views
+from testapp.views import submit_form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('submit/', submit_form, name='submit_form'),
+    path('submit/success.html/',views.success),
+    path('data/',views.index),
 ]
--- Assignment_3/mysite/testapp/views.py
+++ Assignment_3/mysite/testapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import UserInfoForm1
+from django.urls import reverse
+from testapp.models import UserInfo1
 
 # Create your views here.
+def success(request):
+    return render(request,'success.html')
+
+
+def submit_form(request):
+    if request.method == 'POST':
+        form = UserInfoForm1(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success.html')
+# Redirect to a success page
+    else:
+        form = UserInfoForm1()
+    return render(request, 'register.html', {'form': form})
+
+
+
+def index(request):
+    emp_list = UserInfo1.objects.all().count()
+    return HttpResponse(emp_list)
